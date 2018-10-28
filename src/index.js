287fe311e20d14f4e6403f8fc3e57d7f1d88b1c5
spirift <drift.ra@gmail.com>
2018-10-28 21:07:12 +0000

    Setup prisma as a DB for the graphql server

--- src/index.js
+++ src/index.js
@@ -1,50 +1,45 @@
 const { GraphQLServer } = require('graphql-yoga')
-
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-let idCount = links.length
+const { Prisma } = require('prisma-binding')
 
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (root, {id}) => {
-      return links.find((link) => link.id === id)
-    }
+    info: () => `News about pen and paper role playing games`,
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info)
+    },
+    // link: (root, {id}) => {
+    //   return links.find((link) => link.id === id)
+    // },
   },
   Mutation: {
-    post: (root, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      }, info)
     },
-    updateLink: (root, args) => {
-      const linkIndex = links.findIndex((link) => link.id === args.id)
-      if (linkIndex > -1) {
-        if (args.description) {
-          links[linkIndex].description = args.description
-        }
-        if (args.url) {
-          links[linkIndex].url = args.url
-        }
-        return links[linkIndex]
-      }
-    },
-    deleteLink: (root, { id }) => {
-      const linkIndex = links.findIndex((link) => link.id === id)
-      if (linkIndex > -1) {
-        const oldLink = links[linkIndex]
-        links.splice(linkIndex, 1)
-        return oldLink
-      }
-    },
+    // updateLink: (root, args) => {
+    //   const linkIndex = links.findIndex((link) => link.id === args.id)
+    //   if (linkIndex > -1) {
+    //     if (args.description) {
+    //       links[linkIndex].description = args.description
+    //     }
+    //     if (args.url) {
+    //       links[linkIndex].url = args.url
+    //     }
+    //     return links[linkIndex]
+    //   }
+    // },
+    // deleteLink: (root, { id }) => {
+    //   const linkIndex = links.findIndex((link) => link.id === id)
+    //   if (linkIndex > -1) {
+    //     const oldLink = links[linkIndex]
+    //     links.splice(linkIndex, 1)
+    //     return oldLink
+    //   }
+    // },
   },
 }
 
@@ -52,5 +47,15 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://eu1.prisma.sh/michael-7f6e38/pnp-herald/dev',
+      secret: 'drn389sef9t9s-29930snsn-SLQKB-01Hhrl-29N-8Gt',
+      debug: true,
+    }),
+  }),
 })
+
 server.start(() => console.log(`Server is running on http://localhost:4000`))
