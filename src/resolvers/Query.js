5d89faecbf640c4450f25fd69a6b81ccf269a920
spirift <drift.ra@gmail.com>
2018-11-04 13:18:01 +0000

    Add filtering, sorting and pagination to the links feed

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,17 @@
-function feed(root, args, context, info) {
-  return context.db.query.links({}, info)
+function feed(root, { filter, skip, first, orderBy }, context, info) {
+  const where = filter
+    ? {
+        OR: [
+          { url_contains: filter },
+          { description_contains: filter },
+        ],
+      }
+    : {}
+
+  return context.db.query.links(
+    { where, skip, first, orderBy },
+    info,
+  )
 }
 
 module.exports = {
